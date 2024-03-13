28b0cf33db228ee23db890b2f91204eb80794566
Elvijs Leistmanis <s22leiselvi@vea.lv>
2024-03-13 12:03:22 +0200

    Add:Linked Nodes.

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -12,4 +12,22 @@ public class MyLinkedList<T>{
 	public int howManyElements() {
 		return counter;
 	}
+	
+	public void addElement(T element) throws Exception {
+		if (element == null) throw new Exception("Bad input value.");
+		
+		if(isEmpty()) {
+			MyListNode<T> newNode = new MyListNode<T>(element);
+			first = newNode;
+			last = newNode;
+			counter++;
+		}
+		else {
+			MyListNode<T> newNode = new MyListNode<T>(element);
+			newNode.setPrevious(last);
+			last.setNext(newNode);
+			last = newNode;
+			counter++;
+		}
+	}
 }
