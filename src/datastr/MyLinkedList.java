8a87b3b2b5376140a36c1f6bf809b8d1597f6e54
Elvijs Leistmanis <s22leiselvi@vea.lv>
2024-03-13 11:50:09 +0200

    Add:MyLinkedList Class

---
+++ src/datastr/MyLinkedList.java
@@ -0,0 +1,15 @@
+package datastr;
+
+public class MyLinkedList<T>{
+	private MyListNode first = null;
+	private MyListNode last = null;
+	private int counter = 0;
+	
+	public boolean isEmpty() {
+		return (counter == 0);
+	}
+	
+	public int howManyElements() {
+		return counter;
+	}
+}
--- src/datastr/MyListNode.java
+++ src/datastr/MyListNode.java
@@ -1,5 +1,41 @@
 package datastr;
 
-public class MyListNode {
-
+public class MyListNode<T> {
+	private T element;
+	private MyListNode next = null;
+	private MyListNode previous = null;
+	
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T element) {
+		if (element != null)
+			this.element = element;
+		else
+			this.element = (T) new Object();
+	}
+	
+	public MyListNode getNext() {
+		return next;
+	}
+	
+	public void setNext(MyListNode next) {
+		this.next = next;
+	}
+	
+	public MyListNode getPrevious() {
+		return previous;
+	}
+	
+	public void setPrevious(MyListNode previous) {
+		this.previous = previous;
+	}
+	
+	public MyListNode(T element) {
+		setElement(element);
+	}
+	
+	public String toString() {
+		return ""+element;
+	}
 }
